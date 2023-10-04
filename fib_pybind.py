568ad9f37073bd7bddc02a954a2089e5d65aebcc
enriquedlh97 <34756365+enriquedlh97@users.noreply.github.com>
2023-10-04 04:40:44 +0000

    wip

--- fib_pybind.py
+++ fib_pybind.py
@@ -1,5 +1,40 @@
 from time import time
-from fibm_module import fibm
+from fibm_module import fibm, fibonacci_matrix
+import numpy as np
+from numpy.typing import NDArray
+
+
+def fib_np(n: int) -> int:
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return
+    else:
+        matrix: NDArray = np.array(
+            [
+                [1, 1],
+                [1, 0]
+            ],
+            dtype=object
+        )
+
+        result: NDArray = np.linalg.matrix_power(matrix, n - 1)
+
+        return result[0, 0]
+
+    
+def run_python_benchmark_numpy():
+    t0 = time()
+    ans = fib_np(40)
+    t1 = time()
+    print(f'NUMPY: Computed fib(40) = {ans} in {t1 - t0} seconds.')
+
+
+def run_python_benchmark_pybind_matrix():
+    t0 = time()
+    ans = fibonacci_matrix(40)
+    t1 = time()
+    print(f'MATRIX PYBIND: Computed fib(40) = {ans} in {t1 - t0} seconds.')
 
 def run_python_benchmark_pybind():
     t0 = time()
@@ -22,3 +57,7 @@ def run_python_benchmark():
 run_python_benchmark_pybind()
 # Computed fib(40) = 102334155 in 21.669286727905273 seconds.
 run_python_benchmark()
+
+run_python_benchmark_numpy()
+
+run_python_benchmark_pybind_matrix()
--- fibm.cpp
+++ fibm.cpp
@@ -6,6 +6,49 @@ int fibm(int n) {
     return (n < 2) ? n : fibm(n - 1) + fibm(n - 2);
 }
 
+int fibonacci_matrix(int n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        long long int a = 1, b = 1, c = 1, d = 0;
+        long long int temp1, temp2;
+
+        for (int i = 2; i <= n; i++) {
+            temp1 = a * c + b * d;
+            temp2 = b * c + d * (a + b);
+            a = temp1;
+            b = temp2;
+        }
+
+        return b;
+    }
+}
+
+// Wrapper function to call the fibonacci_matrix function with an integer input
+int fibonacci_matrix_wrapper_2(int n) {
+    return fibonacci_matrix(n);
+}
+
+PYBIND11_MODULE(fibm_module_extended, m) {
+    m.def("fibm", &fibm, "Calculate Fibonacci number using C++");
+    m.def("fibonacci_matrix", &fibonacci_matrix_wrapper_2, "Calculate Fibonacci number using matrix exponentiation");
+}
+
+
+
+
+
+
+
+
+// Wrapper function to call the fibonacci_matrix function with an integer input
+int fibonacci_matrix_wrapper(int n) {
+    return fibonacci_matrix(n);
+}
+
 PYBIND11_MODULE(fibm_module, m) {
     m.def("fibm", &fibm, "Calculate Fibonacci number using C++");
+    m.def("fibonacci_matrix", &fibonacci_matrix_wrapper, "Calculate Fibonacci number using matrix exponentiation");
 }
